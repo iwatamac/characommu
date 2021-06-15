d82dcce591d756f32b55608fa9a1c1a31ff32bc5
iwatamac <hallofzv164@gmail.com>
2021-06-15 11:39:31 +0900

    room作成の実装

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,9 +3,22 @@ class PlacesController < ApplicationController
   end
 
   def new
+    @place = Place.new
   end
 
   def create
+    @place = Place.new(place_params)
+    if @place.save
+      redirect_to places_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, character_ids: [])
   end
 
 end
