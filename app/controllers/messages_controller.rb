16b44b5acd2a4e78fd91d27a2579be996411a4ff
iwatamac <hallofzv164@gmail.com>
2021-06-17 15:39:05 +0900

    各roomページにトークするキャラクター情報の表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,9 +2,24 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @place = Place.find(params[:place_id])
+    @characters = Character.includes(:user)
+
   end
 
   def create
+    @place = Place.find(params[:place_id])
+    @message = @place.messages.new(message_params)
+    if @message.save
+      redirect_to place_messages_path(@place)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content).merge(character_id: params[:character_id], place_id: params[:place_id] )
   end
 
 end
