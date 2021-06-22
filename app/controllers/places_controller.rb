c5ac856233acf0bd9c97453b13b99dab96c26e53
iwatamac <hallofzv164@gmail.com>
2021-06-22 12:07:25 +0900

    キャラクター参加機能実装

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -25,6 +25,30 @@ class PlacesController < ApplicationController
     redirect_to places_path
   end
 
+  def edit
+    @place = Place.find(params[:id])
+  end
+
+  def update
+    @place = Place.find(params[:id])
+    @character = Character.find_by(user_id: current_user.id)
+    if @place.characters << @character
+      redirect_to places_path
+    else
+      render :edit
+    end
+  end
+
+  #def join
+    #@place = Place.find_by(id: params[:id])
+    #@character = Character.find_by(user_id: current_user.id)
+    #if !@place.characters.where(user_id: current_user.id)
+      #@place.characters << @character
+      #redirect_to 
+    #end
+  #end
+
+
   private
 
   def place_params
--- db/migrate/20210617082820_rename_character_column_to_messages.rb
+++
@@ -1,4 +0,0 @@
-class RenameCharacterColumnToMessages < ActiveRecord::Migration[6.0]
-  def change
-  end
-end
